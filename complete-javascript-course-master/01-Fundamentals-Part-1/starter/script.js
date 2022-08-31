59fc3b5ef0b60fe1eca4edc74ca813bd2a4b1075
Ismael <ismaeltorresan1@gmail.com>
2022-08-31 12:01:47 -0300

    first commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing'
+if (js === 'amazing') alert('JavaScript is FUN!')
+
+let myName = 'Ismael'
+console.log(myName)
+
+// second time does not need to declare
+myName = 'mamamia'
+
+console.log(typeof myName)
+
+const now = 2022
+const ageMaria = now - 1931
+const myAge = now - 1987
+console.log(ageMaria, myAge)
