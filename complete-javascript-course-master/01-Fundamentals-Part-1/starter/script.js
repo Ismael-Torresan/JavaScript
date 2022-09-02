b0bbb3536cecd33be721bc0127169cfe7484e144
Ismael <ismaeltorresan1@gmail.com>
2022-09-02 16:34:46 -0300

    first episode

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -50,3 +50,26 @@ console.log('23' / '2')
 
 const favourite = prompt('What is your favourite number?')
 console.log(favourite)
+
+// operators
+&& and 
+|| or 
+! not 
+
+
+const day = 'monday'
+
+switch(day) {
+  case 'monday':
+    console.log('Plan course structure')
+    console.log('Go to coding meetup')
+    break
+  case 'tuesday':
+  console.log('prepare theory videos')
+  break
+  case 'wednesday':
+  case 'thursday':
+    console.log('party day')
+  default:
+    console.log('Not a valid day!')
+}
