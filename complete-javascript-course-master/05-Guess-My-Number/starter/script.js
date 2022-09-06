93444c7c4f8fd1da988e2029178bec2a66e29e96
Ismael <ismaeltorresan1@gmail.com>
2022-09-06 11:55:43 -0300

    DOM

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) +1
+let score = Number(20)
+document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess)
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number'
+    }
+    if (score === 0) {
+        document.querySelector('.message').textContent = 'You Lost'
+        score = 0
+    }else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'You won!!! congrats'
+    }else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'you guessed too hight';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'you guessed too low';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+})
