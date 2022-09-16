89dfeed1a958ea2f3195b97991e06e2708d388e6
Ismael <ismaeltorresan1@gmail.com>
2022-09-16 13:00:03 -0300

    minor change

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -140,23 +140,59 @@ imgTargets.forEach(img=>imgObsorver.observe(img))
 const slides = document.querySelectorAll('.slide')
 const btnLeft = document.querySelector('.slider__btn--left')
 const btnRight = document.querySelector('.slider__btn--right')
+const dotContainer = document.querySelector('.dots')
 
 let curSlide = 0
 const maxSlide = slides.length
 
-const slider = document.querySelector('.slider')
-slider.style.transform = 'scale(0.5)'
-slider.style.overflow = 'visible'
+// const slider = document.querySelector('.slider')
+// slider.style.transform = 'scale(0.5)'
+// slider.style.overflow = 'visible'
 
-slides.forEach((s, i) => s.style.transform = `translateX(${100 * i}%)`)
+const createDots = function () {
+  slides.forEach(function(_, i) {
+    dotContainer.insertAdjacentHTML('beforeend',
+     `<button class="dots__dot" data-slide="${i}"></button>`
+     )
+  })
+}
+createDots()
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+}
 
-btnRight.addEventListener('click', function() {
+goToSlide(0)
+
+const nextSlide = function() {
   if(curSlide === maxSlide -1) {
     curSlide = 0
   } else {
     curSlide++
   }
+  goToSlide(curSlide)
+}
 
+const prevSlide = function(){
+  if(curSlide === 0) {
+    curSlide = maxSlide -1
+  } else {
+  curSlide--
+  }
+  goToSlide(curSlide)
+}
 
-  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'ArrowLeft') prevSlide()
+  if(e.key === 'ArrowRight') nextSlide() 
+})
+
+dotContainer.addEventListener('click', function(e){
+  if(e.target.classList.contains('dots__dot')) {
+    console.log('Dot')
+    const {slide} = e.target.dataset
+    goToSlide(slide)
+  }
 })
